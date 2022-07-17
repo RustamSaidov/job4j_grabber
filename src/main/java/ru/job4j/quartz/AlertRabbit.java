2a3e198b8a44b891219f054250d26f4422c06a2d
Qafas <rustam_ussr@mail.ru>
2022-07-17 15:42:04 +0400

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -6,26 +6,24 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-
-    private final Map<String, String> values = new HashMap<String, String>();
-
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
-            AlertRabbit alertRabbit = new AlertRabbit();
-            alertRabbit.load();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(alertRabbit.values.get("rabbit.interval")))
+                    .withIntervalInSeconds(Integer.parseInt(getPropertiesFromPropFile().getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -37,31 +35,19 @@ public class AlertRabbit {
         }
     }
 
-    public void load() {
-        try (BufferedReader read = new BufferedReader(new FileReader("C:\\projects\\job4j_grabber\\src\\main\\resources\\rabbit.properties"))) {
-            for (String line = read.readLine(); line != null; line = read.readLine()) {
-                if (!line.isEmpty()
-                        && line.length() - 1 == line.lastIndexOf("=")
-                        && line.indexOf("=") == line.lastIndexOf("=")
-                        || !line.isEmpty() && 0 == line.indexOf("=")
-                        || line.equals("=")
-                ) {
-                    throw new IllegalArgumentException();
-                }
-                if (line.indexOf("#") != 0 && line.contains("=")) {
-                    String key = line.substring(0, line.indexOf('='));
-                    String val = line.substring(line.indexOf('=') + 1);
-                    values.put(key, val);
-                }
-            }
+    private static Properties getPropertiesFromPropFile() {
+        Properties config = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(in);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        return config;
     }
 
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
         }
     }

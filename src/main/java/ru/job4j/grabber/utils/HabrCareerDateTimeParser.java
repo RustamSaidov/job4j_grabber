195401a9522e65dae5c78da074545ac087794fab
rusta <rustam_ussr@mail.ru>
2022-07-21 09:24:39 +0300

    2.1. Преобразование даты [#289476 #323765]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -6,8 +6,6 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        ZonedDateTime zdt = ZonedDateTime.parse(parse);
-        ZoneId zId = ZoneId.of("Europe/Moscow");
-        return LocalDateTime.ofInstant(zdt.toInstant(), zId);
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 }

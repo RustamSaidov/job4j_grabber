0ab0e62387138d4886cb9f6dbff6231715f775d3
rusta <rustam_ussr@mail.ru>
2022-07-22 10:50:05 +0300

    4. Схема таблицы Post [#1731]

---
+++ sql/post.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE public.post
+(
+   id serial primary key,   	
+   name character varying(255), 
+   text character varying, 
+   link character varying UNIQUE, 
+   created timestamptz
+);

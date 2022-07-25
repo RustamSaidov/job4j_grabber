ec7a311f8d43224d2756b2be57738cdf351100f8
rusta <rustam_ussr@mail.ru>
2022-07-25 09:02:31 +0300

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -78,9 +78,7 @@ public class Grabber implements Grab {
                     try (OutputStream out = socket.getOutputStream()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         for (Post post : store.getAll()) {
-
-                            out.write
-                                    (post.toString().getBytes());
+                            out.write(post.toString().getBytes());
                             out.write(System.lineSeparator().getBytes());
                         }
                     } catch (IOException io) {
